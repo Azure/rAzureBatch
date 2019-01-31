897593da49105212dd39ed559b4c825bb21737f7
Brian <l.brian.hoang@gmail.com>
2019-01-31 15:37:59 -0800

    Added http traffic support

--- R/azure_request.R
+++ R/azure_request.R
@@ -60,6 +60,12 @@ AzureServiceClient <- R6::R6Class(
     executeRequest = function(url, request) {
       requestHeaders <- httr::add_headers(request$headers)
 
+      verbose <- options("azureHttpTraffic")
+      if (!is.null(verbose$azureHttpTraffic)
+          && verbose$azureHttpTraffic == TRUE) {
+        request$verbose <- httr::verbose()
+      }
+
       if (request$method == "GET" ||
           request$method == "POST" ||
           request$method == "DELETE" ||
